37a1932ffa4c08357a137e4a27cd27efb8d34482
dezska <104264903+antiglory@users.noreply.github.com>
2024-08-21 23:50:18 -0300

    change of structure

--- source/Struct/gdt32.asm
+++ source/Struct/gdt32.asm
@@ -1,16 +1,19 @@
 ; setting up GDT for 32 BIT MODE
 GDT32:
-    .null: equ $ - GDT32
+    .null:
         dd 0x0, 0x0
 
-    .code: equ $ - GDT32
-        dw 0xffff, 0x0      
-        db 0x0, 10011010b, 11001111b, 0x0
-
-    .data: equ $ - GDT32
+    .code:
         dw 0xffff, 0x0
-        db 0x0, 10010010b, 11001111b, 0x0
+        db 0x0, 0b10011010, 0b11001111, 0x0
 
-    .ptr:                    
+    .data:
+        dw 0xffff, 0x0
+        db 0x0, 0b10010010, 0b11001111, 0x0
+
+    .descriptor:
         dw $ - GDT32 - 1
         dd GDT32
+
+    .code_ptr equ GDT32.code - GDT32
+    .data_ptr equ GDT32.data - GDT32
