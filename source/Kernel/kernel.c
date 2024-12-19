9196a93f314fcd5cc407b9a199ddd651837abddb
dezska <104264903+antiglory@users.noreply.github.com>
2024-12-19 16:27:21 -0300

    update kernel.c

--- source/Kernel/kernel.c
+++ source/Kernel/kernel.c
@@ -1,27 +1,50 @@
-void _start() __attribute__((section(".text.boot"))) __attribute__((naked));
+void _start()   __attribute__((section(".text.boot"))) __attribute__((naked));
+void halt()     __attribute__((noreturn));
+
+extern char __stack_start[];
+extern char __stack_end[];
 
 void halt()
 {
-    asm volatile (
-        "hlt \n\t"
-    );
-
-    return; // ?
+    while(1) {
+        asm volatile (
+            "cli\n\t"
+            "hlt\n\t"
+        );
+    }
 }
 
-// kernel print not formatted
-void kprintnf(unsigned char* str) {
-    unsigned short* vmem = (unsigned short*)0xB8000;
-    unsigned short ref = 0x1F00;
+// kernel print not formatted (WIP)
+void kprintnf(unsigned char* str)
+{
+    unsigned short* vmem = (unsigned short*)0xFFFFFF80000B8000; // canonical form of VGA mem
+    unsigned short  ref = 0x1F00;
 
     for (char i = 0; str[i] != '\0'; i++) {
         vmem[i] = (ref | str[i]);
     }
 }
 
+// kernel entrypoint
 void _start()
 {
-    kprintnf("a l i v e");
+    // setup stack
+    asm volatile (
+        "movq %0, %%rax\n\t"
+        "movq %%rax, %%rsp\n\t"
+        "movq %%rsp, %%rbp\n\t"
+        "jmp main\n\t"
+        : 
+        : "r"(__stack_end)
+        : "rax", "memory"
+    );
+
+    __builtin_unreachable();
+}
+
+void main()
+{
+    kprintnf("Greetz! - v0.1");
 
     halt();
 }
