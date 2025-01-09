536e0db947bae2d34ce0ce6c3b5e4275282e50ac
dezska <104264903+antiglory@users.noreply.github.com>
2025-01-08 21:05:44 -0300

    update kernel.c

--- source/Kernel/kernel.c
+++ source/Kernel/kernel.c
@@ -1,44 +1,32 @@
-void _kstart()  __attribute__((section(".text.boot"))) __attribute__((naked));
-void halt()     __attribute__((noreturn));
+#include <stdint.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#define TEXT    __attribute__((section(".text")))
+#define BTEXT   __attribute__((section(".text.boot")))
+#define ALIGNED __attribute__((aligned(1)))
+
+BTEXT ALIGNED void kstart()  __attribute__((naked));
+TEXT  ALIGNED void init();
 
 extern char __stack_start[];
 extern char __stack_end[];
 
-void halt()
-{
-    while(1)
-    {
-        asm volatile
-        (
-            "cli\n\t"
-            "hlt\n\t"
-        );
-    }
-}
-
-// kernel print not formatted (WIP)
-void kprintnf(unsigned char* str)
-{
-    unsigned short* vmem = (unsigned short*)0xFFFFFF80000B8000; // canonical form of VGA mem
-    unsigned short  ref  = 0x1F00;
-
-    for (char i = 0; str[i] != '\0'; i++)
-    {
-        vmem[i] = (ref | str[i]);
-    }
-}
-
 // kernel entrypoint
-void _kstart()
+void kstart(void)
 {
     // setup stack
     asm volatile
     (
+        "cli\n\t"
         "movq %0, %%rax\n\t"
         "movq %%rax, %%rsp\n\t"
         "movq %%rsp, %%rbp\n\t"
-        "jmp main\n\t"
-        : 
+
+        "jmp init\n\t"
+        :
         : "r"(__stack_end)
         : "rax", "memory"
     );
@@ -46,10 +34,14 @@ void _kstart()
     __builtin_unreachable();
 }
 
-void main()
+#include "modules/wrappers.h"
+#include "modules/io.h"
+// #include "modules/init.h"
+
+void init(void)
 {
-    // 0xffffff8000000000 - 0xffffff8000200000 [pt_ffffff8000000]
-    kprintnf("Greetz! - v0.1");
+    // 0xffffff8000000000 - 0xffffff8000200000 [pt_ffffff8000000]    
+    kprintnf("Greetz! - v0.1\n");
 
     halt();
 }
