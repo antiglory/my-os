38e7191e6d929a8a2d4ee6ed14f8ef90d77c597f
dezska <104264903+antiglory@users.noreply.github.com>
2025-07-28 15:55:14 -0300

    update macro.h

--- source/Kernel/modules/macro.h
+++ source/Kernel/modules/macro.h
@@ -3,15 +3,9 @@
 
 #define BTEXT   __attribute__((section(".text.boot")))
 #define TEXT    __attribute__((section(".text")))
+
 #define ALIGNED __attribute__((aligned(1)))
 
-#define JMP(addr)                   \
-    asm volatile                    \
-    (                               \
-        "jmp *%0\n\t"               \
-        :                           \
-        : "r" ((void*)addr)         \
-        :                           \
-    )
+#define GDT64_CODE_PTR 0x08
 
 #endif
