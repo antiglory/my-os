929a60c6509eb0fd283eba1d3a1c8e513e34b647
dezska <104264903+antiglory@users.noreply.github.com>
2025-07-28 16:00:26 -0300

    update wrapper.h

--- source/Kernel/modules/wrapper.h
+++ source/Kernel/modules/wrapper.h
@@ -3,6 +3,15 @@
 
 TEXT  ALIGNED void halt(void)    __attribute__((noreturn));
 
+#define jmp(addr)                   \
+    asm volatile                    \
+    (                               \
+        "jmp *%0\n\t"               \
+        :                           \
+        : "r" ((void*)addr)         \
+        :                           \
+    )                               \
+
 void halt(void)
 {
     while(1)
@@ -13,6 +22,16 @@ void halt(void)
             "hlt\n\t"
         );
     }
+}
+
+inline void outb(uint16_t port, uint8_t value)
+{
+    asm volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
+inline void in(uint16_t port, uint8_t value)
+{
+    asm volatile ("in %0, %1" : : "a"(value), "Nd"(port));
 }
 
 #endif
