36106f30848ee538b7f1eed7fb12fc391e406518
dezska <104264903+antiglory@users.noreply.github.com>
2025-07-28 15:59:19 -0300

    update map.h

--- source/Kernel/modules/init/map.h
+++ source/Kernel/modules/init/map.h
@@ -8,14 +8,21 @@ extern uint64_t* pd_table;
 extern uint64_t* pt_tables;
 
 // refs to sections
+extern char _kernel_text_boot_start[];
+extern char _kernel_text_boot_end[];
+
 extern char _kernel_text_start[];
 extern char _kernel_text_end[];
+
 extern char _kernel_rodata_start[];
 extern char _kernel_rodata_end[];
+
 extern char _kernel_data_start[];
 extern char _kernel_data_end[];
+
 extern char _kernel_bss_start[];
 extern char _kernel_bss_end[];
+
 extern char _kernel_stack_start[];
 extern char _kernel_stack_end[];
 
@@ -35,65 +42,86 @@ static void map_range (uint64_t virt_start, uint64_t phys_start,
     uint64_t phys_addr = phys_start;
     
     // calculate PT index for the virtual address
-    uint64_t pt_idx = (virt_start >> 12) & 0x1FF;
+    uint64_t pt_i = (virt_start >> 12) & 0x1FF;
     
     while (virt_start < virt_end)
     {
         // setup PTE
         uint64_t pte = phys_addr | flags | PTE_PRESENT;
-        pt_tables[pt_idx] = pte;
+        pt_tables[pt_i] = pte;
         
         virt_start += PAGE_SIZE;
         phys_addr  += PAGE_SIZE;
-        pt_idx++;
+        pt_i++;
         
         // if reachs the end of current PT, break
-        if (pt_idx >= ENTRIES_PER_PT)
+        if (pt_i >= ENTRIES_PER_PT)
             break;
     }
 }
 
-void setup_kernel_paging(void)
+void init_map_kernel_sections(void)
 {
+    map_range
+    (
+        (uint64_t)_kernel_text_boot_start,
+        (uint64_t)_kernel_text_boot_start,
+        (uint64_t)(_kernel_text_boot_end - _kernel_text_boot_start),
+        PTE_PRESENT
+    );
+
     // .text - read-only, executable
-    map_range(
+    map_range
+    (
         (uint64_t)_kernel_text_start,
         (uint64_t)_kernel_text_start,
         (uint64_t)(_kernel_text_end - _kernel_text_start),
         PTE_PRESENT
     );
+    *(uintptr_t*)(_kernel_text_start + (unsigned char)1);
+    kprintf("[ BOOT ] text -> start %p ; end %p\n", _kernel_text_start, _kernel_text_end);
     
     // .rodata - read-only, non-executable
-    map_range(
+    map_range
+    (
         (uint64_t)_kernel_rodata_start,
         (uint64_t)_kernel_rodata_start,
         (uint64_t)(_kernel_rodata_end - _kernel_rodata_start),
         PTE_PRESENT | PTE_NX
     );
+    *(uintptr_t*)(_kernel_rodata_start + (unsigned char)1);
     
     // .data - read-write, non-executable
-    map_range(
+    map_range
+    (
         (uint64_t)_kernel_data_start,
         (uint64_t)_kernel_data_start,
         (uint64_t)(_kernel_data_end - _kernel_data_start),
         PTE_PRESENT | PTE_WRITABLE | PTE_NX
     );
+    *(uintptr_t*)(_kernel_data_start + (unsigned char)1);
+    kprintf("[ BOOT ] data -> start %p ; end %p\n", _kernel_data_start, _kernel_data_end);
     
     // .bss - read-write, non-executable
-    map_range(
+    map_range
+    (
         (uint64_t)_kernel_bss_start,
         (uint64_t)_kernel_bss_start,
         (uint64_t)(_kernel_bss_end - _kernel_bss_start),
         PTE_PRESENT | PTE_WRITABLE | PTE_NX
     );
+    *(uintptr_t*)(_kernel_bss_start + (unsigned char)1);
     
     // .stack - read-write, non-executable
-    map_range(
+    map_range
+    (
         (uint64_t)_kernel_stack_start,
         (uint64_t)_kernel_stack_start,
         (uint64_t)(_kernel_stack_end - _kernel_stack_start),
         PTE_PRESENT | PTE_WRITABLE | PTE_NX
     );
+    *(uintptr_t*)(_kernel_stack_end - (unsigned char)1);
+    kprintf("[ BOOT ] stack -> start %p ; end %p\n", _kernel_stack_start, _kernel_stack_end);
 }
 
 #endif
