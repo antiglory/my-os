0610f9cd5afc7bf2e748ce65013bf1a0d1b638f1
dezska <104264903+antiglory@users.noreply.github.com>
2025-07-28 15:57:34 -0300

    update init.h

--- source/Kernel/modules/init.h
+++ source/Kernel/modules/init.h
@@ -3,5 +3,17 @@
 
 #include "init/map.h"
 #include "init/idt.h"
+#include "init/pic.h"
+#include "init/pit.h"
+
+void init(void)
+{
+    init_pic();
+    init_pit();
+
+    asm volatile("sti\n\t");
+
+    jmp(&main);
+}
 
 #endif
